3254314c068d41b458381fd5b29d1df9566cd792
Mickael <mickael.rolo@gmail.com>
2017-10-04 13:54:56 +0200

    add widget twitter

--- src/app/portal/portal.module.ts
+++ src/app/portal/portal.module.ts
@@ -3,12 +3,14 @@ import { NgModule }            from '@angular/core';
 import { SharedModule }        from '../shared/shared.module';
 
 import { PortalComponent }       from './portal.component';
+import { TwitterComponent }       from './twitter.component';
 import { PortalRoutingModule }   from './portal-routing.module';
 
 @NgModule({
   imports: [ SharedModule, PortalRoutingModule ],
   declarations: [
-    PortalComponent
+    PortalComponent,
+    TwitterComponent
   ]
 })
 export class PortalModule { }
